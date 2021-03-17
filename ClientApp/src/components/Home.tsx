60b29a2216efc3c8e04b32c8829e32abaceb33ff
Emmanuel Enyinnaya <eenyinnaya@swiftng.net>
2021-03-17 02:52:34 +0100

    update

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => (
     <h1>Welcome!</h1>
     <p>Welcome to movie Project</p>
     <ul>
-            <li><a href='https://localhost:44386/swagger'>Click</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'></a> To view a list of the available endpoint</li>
+            <li><a href='https://movieproject20210317020108.azurewebsites.net/swagger'>Click</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'></a> To view a list of the available endpoint</li>
      
     </ul>
    
